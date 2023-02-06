d99d3c2dd0408092935a362c53fd0977a3f3536e
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-07 00:19:05 +0500

    FEB-7

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -4,32 +4,82 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen, PushNotifications } from "../Screens";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useEffect } from "react";
-import * as Notifications from "expo-notifications";
-import { useNotifications } from "../Hooks/useNotifications";
+// import * as Notifications from "expo-notifications";
+// import { useNotifications } from "../Hooks/useNotifications";
+import messaging from "@react-native-firebase/messaging";
 
 const Stack = createNativeStackNavigator();
 
 export const Navigator = () => {
-  const { registerForPushNotificationsAsync, handleNotificationResponse } =
-    useNotifications();
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-    Notifications.setNotificationHandler({
-      handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: true,
-        shouldSetBadge: true,
-      }),
-    });
-    const responseListener =
-      Notifications.addNotificationResponseReceivedListener(
-        handleNotificationResponse
-      );
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-    return () => {
-      if (responseListener)
-        Notifications.removeNotificationSubscription(responseListener);
-    };
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+
+  // const { registerForPushNotificationsAsync, handleNotificationResponse } =
+  //   useNotifications();
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    } else {
+      console.log("Failed Token Status");
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+        }
+      });
+
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage.notification
+      );
+    });
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+
+    // registerForPushNotificationsAsync();
+    // Notifications.setNotificationHandler({
+    //   handleNotification: async () => ({
+    //     shouldShowAlert: true,
+    //     shouldPlaySound: true,
+    //     shouldSetBadge: true,
+    //   }),
+    // });
+    // const responseListener =
+    //   Notifications.addNotificationResponseReceivedListener(
+    //     handleNotificationResponse
+    //   );
+
+    // return () => {
+    //   if (responseListener)
+    //     Notifications.removeNotificationSubscription(responseListener);
+    // };
   }, []);
 
   return (
